0df316b664f366f8b86ff798a779f0ba729f7895
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-04 14:28:43 +0500

    Setup Random Greetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  #get 'root/index'
+  get 'api/greetings'
+  # get 'root/index'
   root 'root#index'
+
+  namespace :api do
+    get 'messages', to: 'api#messages'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
